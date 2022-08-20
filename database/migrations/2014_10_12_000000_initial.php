83f4fceda2d86d5bca462f70e36d0b033cea91d8
Romi Kusuma Bakti <romikusumab@gmail.com>
2022-08-20 22:39:41 +0700

    fix migration

--- database/migrations/2014_10_12_000000_initial.php
+++ database/migrations/2014_10_12_000000_initial.php
@@ -22,7 +22,6 @@ return new class extends Migration
             $table->string('nim')->unique();
             $table->string('gender');
             $table->date('date_of_birth');
-            $table->foreign()
             $table->rememberToken();
             $table->timestamps();
         });
